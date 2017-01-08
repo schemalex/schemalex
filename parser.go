1a519b7519c3db72e6cdf0df82018ff13d5d7c3b
Daisuke Maki <lestrrat+github@gmail.com>
2017-01-08 23:34:44 +0900

    move and remove

--- parser.go
+++ parser.go
@@ -8,6 +8,31 @@ import (
 	"golang.org/x/net/context"
 )
 
+const (
+	coloptSize = 1 << iota
+	coloptDecimalSize
+	coloptDecimalOptionalSize
+	coloptUnsigned
+	coloptZerofill
+	coloptBinary
+	coloptCharacterSet
+	coloptCollate
+	coloptNull
+	coloptDefault
+	coloptAutoIncrement
+	coloptKey
+	coloptComment
+)
+
+const (
+	coloptFlagNone            = 0
+	coloptFlagDigit           = coloptSize | coloptUnsigned | coloptZerofill
+	coloptFlagDecimal         = coloptDecimalSize | coloptUnsigned | coloptZerofill
+	coloptFlagDecimalOptional = coloptDecimalOptionalSize | coloptUnsigned | coloptZerofill
+	coloptFlagTime            = coloptSize
+	coloptFlagChar            = coloptSize | coloptBinary | coloptCharacterSet | coloptCollate
+	coloptFlagBinary          = coloptSize
+)
 // Parser is responsible to parse a set of SQL statements
 type Parser struct{}
 
