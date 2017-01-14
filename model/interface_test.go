41312ee75d99955df8c9044bd9a4f01b8dce2209
Daisuke Maki <lestrrat+github@gmail.com>
2017-01-14 16:59:13 +0900

    fix

--- model/interface_test.go
+++ model/interface_test.go
@@ -6,9 +6,11 @@ func TestInterfaces(t *testing.T) {
 	{
 		var stmt Index
 		stmt = &index{}
+		_ = stmt
 	}
 	{
 		var stmt TableColumn
-		stmt= &tablecol{}
+		stmt = &tablecol{}
+		_ = stmt
 	}
 }
