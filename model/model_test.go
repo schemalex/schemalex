c9ab215c9e585dcdf48f3072a3259cb404a2fbdf
Kensaku Araga <ken39arg@gmail.com>
2017-02-17 12:05:22 +0900

    fix `model/model_test.go:15: not enough arguments in call to model.NewIndex`

--- model/model_test.go
+++ model/model_test.go
@@ -12,5 +12,5 @@ func TestStatement(t *testing.T) {
 	stmts = append(stmts, model.NewDatabase("test"))
 	stmts = append(stmts, model.NewTable("test"))
 	stmts = append(stmts, model.NewTableColumn("test"))
-	stmts = append(stmts, model.NewIndex(model.IndexKindPrimaryKey))
+	stmts = append(stmts, model.NewIndex(model.IndexKindPrimaryKey, stmts[1].ID()))
 }
