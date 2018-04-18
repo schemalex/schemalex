021814b035175449f6b953d6e9b2dbd20a92a9a3
Ichinose Shogo <shogo82148@gmail.com>
2018-04-18 11:55:33 +0900

    generate files using newer stringer

--- model/index_kind_string_gen.go
+++ model/index_kind_string_gen.go
@@ -2,7 +2,7 @@
 
 package model
 
-import "fmt"
+import "strconv"
 
 const _IndexKind_name = "IndexKindInvalidIndexKindPrimaryKeyIndexKindNormalIndexKindUniqueIndexKindFullTextIndexKindSpatialIndexKindForeignKey"
 
@@ -10,7 +10,7 @@ var _IndexKind_index = [...]uint8{0, 16, 35, 50, 65, 82, 98, 117}
 
 func (i IndexKind) String() string {
 	if i < 0 || i >= IndexKind(len(_IndexKind_index)-1) {
-		return fmt.Sprintf("IndexKind(%d)", i)
+		return "IndexKind(" + strconv.FormatInt(int64(i), 10) + ")"
 	}
 	return _IndexKind_name[_IndexKind_index[i]:_IndexKind_index[i+1]]
 }
